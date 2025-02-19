0cf740a923b921d351fdf60d8441a071f038b614
rayan <rayancooray123456@gmail.com>
2025-02-19 15:20:17 +0530

    Bank Card and routes done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,29 @@
+
+import MobileNav from '@/components/MobileNav'
+import Sidebar from '@/components/Sidebar'
+import Image from 'next/image'
 import React from 'react'
 
 const layout = ({ children }:
     { children: React.ReactNode }) =>
 {
+
+  const loggedInUser = {firstName: 'Ryan', lastName:'CHL Cooray'}
   return (
-    <main>
-        SIDEBAR
-        {children}
-    </main>
+
+    <main className="flex h-screen w-full font-inter">
+    <Sidebar user={loggedInUser} />
+
+    <div className="flex size-full flex-col">
+      <div className="root-layout">
+        <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+        <div>
+          <MobileNav user={loggedInUser} />
+        </div>
+      </div>
+      {children}
+    </div>
+  </main>
   )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,40 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName : 'Ryan' , lastName : 'CH' , email : 'G2SbK@example.com' }
+
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+        <div className='home-content'>
+          <header className='home-content'>
+            <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || 'Guest'}
+            subtext="Access and manage your accounts and transactions effortlessly."
+            />
+
+            <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.45} 
+            />
+            </header>
+
+            RECENT TRANSACTIONS
+        </div>
+
+      <RightSideBar 
+      user={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance: 250.50},
+        {cuurentBalance: 250.50}]}
+      />
+
+    </section>
   )
 }
 
