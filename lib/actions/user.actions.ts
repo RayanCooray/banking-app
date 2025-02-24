f0b27e96799c451c1ddeda910176797a5f9e8dd7
rayan <rayancooray123456@gmail.com>
2025-02-24 10:44:42 +0530

    done

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -57,7 +57,7 @@ export const logoutAccount = async () => {
   try {
     const { account } = await createSessionClient();
 
-    cookies().delete('appwrite-session');
+    (await cookies()).delete('appwrite-session');
 
     await account.deleteSession('current');
   } catch (error) {
